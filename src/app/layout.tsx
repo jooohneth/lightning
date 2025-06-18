62b92d620be3ac7a3999764dd2e1009f6c43e023
jooohneth <jooohn.eth@gmail.com>
2025-06-19 01:07:04 +0200

    chore: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,10 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Lightning Grants",
-  description: "The only way to get funded.",
+  description: "First to Back the First to Build.",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -19,6 +22,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta
+        property="twitter:image"
+        content="https://lightning-six.vercel.app/opengraph.png"
+      />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:title" content="Lightning Grants" />
+      <meta
+        property="twitter:description"
+        content="Lightning Grants: First to Back the First to Build."
+      />
+
+      <meta
+        property="og:image"
+        content="https://lightning-six.vercel.app/opengraph.png"
+      />
+      <meta property="og:site_name" content="Lightning Grants" />
+      <meta property="og:title" content="Lightning Grants" />
+      <meta
+        property="og:description"
+        content="First to Back the First to Build."
+      />
+      <meta property="og:url" content="https://lightning-six.vercel.app"></meta>
       <body className={`${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
