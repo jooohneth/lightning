04aa48418caa8fe39c795721fa81ff276f3ab9ee
jooohneth <jooohn.eth@gmail.com>
2025-06-19 10:39:53 +0200

    chore: upd gradients

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -10,20 +10,20 @@ export const data: Data[] = [
     heading: "Fast",
     title: "Because the best ideas can't wait",
     description: "We review all new applications weekly.",
-    img: "/card/1.jpg",
+    img: "/card/1.jpeg",
   },
   {
     heading: "Easy",
     title: "< 10min Application",
     description:
       "Ditch the Pitch. We just wanna know what you're building and why.",
-    img: "/card/2.jpg",
+    img: "/card/2.jpeg",
   },
   {
     heading: "Public",
     title: "Every grant becomes a story",
     description:
       "We spotlight you through thoughtful content on our channels. We're here to help you grow.",
-    img: "/card/3.jpg",
+    img: "/card/3.jpeg",
   },
 ] as const;
